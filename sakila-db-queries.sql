d62fe3b9bb1c069201d5c360156942e45a6e7f6f
Joel Sotelo <joel.pilgrim88@gmail.com>
2019-01-31 23:53:07 -0600

    Added solution for 7F to 7H

--- sakila-db-queries.sql
+++ sakila-db-queries.sql
@@ -159,26 +159,157 @@ order by cust.last_name;
 
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
 
+select title
+from film 
+where (title like 'K%' or title like 'Q%')
+and language_id in (
+	select language_id 
+	from language 
+	where name = 'English'
+)
+order by title;
+
 
 -- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
 
+select first_name, last_name 
+from actor
+where actor_id in (
+	select actor_id
+	from film_actor
+	where film_id in (
+		select film_id from film where lower(title) = lower('Alone Trip')
+	)
+);
 
 -- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+
+-- Subquery
+select first_name, last_name, email
+from customer
+where address_id in (
+	select address_id
+	from address
+	where city_id in (
+		select city_id
+		from city
+		where country_id in (
+			select country_id
+			from country
+			where country = 'Canada'
+		)
+	)
+);
+
+-- Join
+
+select cus.first_name, cus.last_name, cus.email
+from customer cus
+join address adr
+on cus.address_id = adr.address_id
+join city cit
+on adr.city_id = cit.city_id
+join country cou
+on cit.country_id = cou.country_id
+where cou.country = 'Canada';
+
+
 
 
 -- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
 
+select film_id, title, release_year
+from film
+where film_id in (
+	select film_id
+	from film_category
+	where category_id in (
+		select category_id
+		from category
+		where name = 'Family'
+	)
+);
 
 -- 7e. Display the most frequently rented movies in descending order.
 
+select A.film_id, A.title, B.*
+from film A
+join (
+	select inv.film_id, count(ren.rental_id) times_rented
+	from rental ren
+	join inventory inv
+	on ren.inventory_id = inv.inventory_id
+	group by inv.film_id
+) B
+on A.film_id = B.film_id
+order by B.times_rented desc;
 
 -- 7f. Write a query to display how much business, in dollars, each store brought in.
 
+select A.store_id, B.sales 
+from store A
+join (
+	select cus.store_id, sum(pay.amount) sales
+	from customer cus
+	join payment pay
+	on pay.customer_id = cus.customer_id
+group by cus.store_id
+) B
+on A.store_id = B.store_id
+order by a.store_id;
 
 -- 7g. Write a query to display for each store its store ID, city, and country.
 
+select sto.store_id, cit.city, cou.country
+from store sto
+left join address adr
+on sto.address_id = adr.address_id
+join city cit
+on adr.city_id = cit.city_id
+join country cou
+on cit.country_id = cou.country_id;
+
+select A.*, B.sales 
+from (
+	select sto.store_id, cit.city, cou.country
+	from store sto
+	left join address adr
+	on sto.address_id = adr.address_id
+	join city cit
+	on adr.city_id = cit.city_id
+	join country cou
+	on cit.country_id = cou.country_id
+) A
+join (
+	select cus.store_id, sum(pay.amount) sales
+	from customer cus
+	join payment pay
+	on pay.customer_id = cus.customer_id
+	group by cus.store_id
+) B
+on A.store_id = B.store_id
+order by a.store_id;
+
+
 
 -- 7h. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+
+
+select cat.name, sum( IFNULL(pay.amount, 0) ) amount
+from category cat
+left join film_category flm_cat
+on cat.category_id = flm_cat.category_id
+left join film fil
+on flm_cat.film_id = fil.film_id
+left join inventory inv
+on fil.film_id = inv.film_id
+left join rental ren
+on inv.inventory_id = ren.inventory_id
+left join payment pay
+on ren.rental_id = pay.rental_id
+group by cat.name
+order by revenue desc
+limit 5;
 
 
 -- 8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
